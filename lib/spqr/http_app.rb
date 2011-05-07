790c46ac6d764545ca4739d40f4e40b96fb7c333
Will Benton <willb@redhat.com>
2011-05-07 10:03:50 -0500

    WIP on http_app

--- lib/spqr/http_app.rb
+++ lib/spqr/http_app.rb
@@ -3,7 +3,7 @@
 # HTTP/JSON-RPC application skeleton class.  Maybe best described as
 # "RES", since it's about 75% RESTful.
 #
-# Copyright (c) 2009--2010 Red Hat, Inc.
+# Copyright (c) 2009--2011 Red Hat, Inc.
 #
 # Author:  William Benton (willb@redhat.com)
 # 
@@ -15,5 +15,49 @@
 
 module SPQR
   class HttpApp
+    def initialize(options=nil)
+            defaults = {:logfile=>STDERR, :loglevel=>Logger::WARN, :notifier=>nil, :port=>9529}
+      
+      # convenient shorthands for log levels
+      loglevels = {:debug => Logger::DEBUG, :info => Logger::INFO, :warn => Logger::WARN, :error => Logger::ERROR, :fatal => Logger::FATAL}
+        
+      options = defaults unless options
+
+      # set unsupplied options to defaults
+      defaults.each do |k,v|
+        options[k] = v unless options[k]
+      end
+
+      # fix up shorthands
+      options[:loglevel] = loglevels[options[:loglevel]] if loglevels[options[:loglevel]]
+
+      logger_opts = ([options[:logfile]] + [options[:logoptions]]).flatten.compact
+
+      @log = Logger.new(*logger_opts)
+      @log.level = options[:loglevel]
+
+      @log.info("initializing SPQR app....")
+
+      @event_classes = []
+      @classes_by_name = {}
+      @classes_by_id = {}
+      @pipe = options[:notifier]
+      @app_name = (options[:appname] or "SPQR application [#{Process.pid}]")
+      
+    end
+
+    def register(*ks)
+      manageable_ks = ks.select {|kl| manageable? kl}
+      unmanageable_ks = ks.select {|kl| not manageable? kl}
+
+      manageable_ks.each do |klass|
+        
+      end
+    end
+
+    private
+    def camel2url(klass)
+      klass.to_s.split('::').pop.gsub(/([A-Z])([A-Z])([a-z])/, '\1_\2\3').gsub(/([a-z])([A-Z])/, '\1_\2').downcase
+    end
   end
 end
